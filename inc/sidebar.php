8d8050b88b6b4605ca85a3531f9a59571d7f19a1
Rosita Cabrera <rographx@bellsouth.net>
2012-04-26 00:02:25 -0400

    sync any changes

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -20,4 +20,23 @@
      	<li><a href="info.php">PHP info page</a></li>
         <li><a href="http://www.ipage.com/ipage/index.html">iPage Web Hosting</a></li> 
      </ul>
+     
+     <br />
+     
+     <p>
+    
+    
+ <p>
+    <a href="http://validator.w3.org/check?uri=http%3A%2F%2Frodesignsatl.com%2Frcabrera410_Jump%2Findex.html"><img
+      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
+
+<a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Frodesignsatl.com%2Frcabrera410_Jump%2Findex.html">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+</a>
+  </p>
+     
+
+  </p>
 </div>
